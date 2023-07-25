92f5a66c5c04d45b3a6f4add6ef7dfaa0df1d9e0
Ramon-Machado91 <ramonmachado383@yahoo.com.br>
2023-07-24 23:33:39 -0300

    slide completed

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-let listaDeSlides = document.querySelectorAll('#slides-container')
-let setaAvancar = document.getElementById('avancar')
-let setaVoltar = document.getElementById('voltar')
+let listaDeSlides = document.querySelectorAll(".slide");
+let setaAvancar = document.getElementById("avancar");
+let setaVoltar = document.getElementById("voltar");
 
 let imagemAtual = 0;
 
-setaAvancar.addEventListener('click',function(){
+setaAvancar.addEventListener("click", function () {
+  if (imagemAtual === listaDeSlides.length - 1) {
+    return;
+  }
 
-    esconderImagemAberta()
+  esconderImagemAberta();
+  imagemAtual++;
+  listaDeSlides[imagemAtual].classList.add("mostrar");
 
-    console.log(listaDeSlides)
-})
+  mostrarEsconderSetas();
+});
 
-function esconderImagemAberta(){
-    const imagemAberta = document.querySelector('.mostrar');
-    imagemAberta.classList.remove('mostrar')
-    
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual === 0) {
+    return;
+  }
+
+  esconderImagemAberta();
+  imagemAtual--;
+  listaDeSlides[imagemAtual].classList.add("mostrar");
+
+  mostrarEsconderSetas();
+});
+
+function esconderImagemAberta() {
+  const imagemAberta = document.querySelector(".mostrar");
+  imagemAberta.classList.remove("mostrar");
+}
+
+function mostrarEsconderSetas() {
+  const naoPrimeiraImagem = imagemAtual != 0;
+  if (naoPrimeiraImagem) {
+    setaVoltar.classList.remove("opacidade");
+  } else {
+    setaVoltar.classList.add("opacidade");
+  }
+
+  const ultimaImagem =
+    imagemAtual != 0 && imagemAtual === listaDeSlides.length - 1;
+  if (ultimaImagem) {
+    setaAvancar.classList.add("opacidade");
+  } else {
+    setaAvancar.classList.remove("opacidade");
+  }
 }
